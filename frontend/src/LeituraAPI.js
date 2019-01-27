664d115ad3863489e5f471d13470c2cbc459bc69
Daniela Duarte <falecom@danieladuarte.com.br>
2019-01-27 17:27:06 -0200

    refactor: add method in fetchs

--- frontend/src/LeituraAPI.js
+++ frontend/src/LeituraAPI.js
@@ -1,49 +1,70 @@
 const api = "http://localhost:3001";
 
-const headers ={
+const headers = {
     'Accept': 'application/json',
     'Authorization': 'whatever-you-want'
 }
 
 export const getAllPosts = () =>
-  fetch(`${api}/posts`, { headers })
-    .then(res => res.json())
-    .then(data => {
-        //console.log('todos os posts')
-        //console.log(data)
-    return data
-    })
-    .catch(error =>{
-        console.log(error)
-        return error
-    })
+    fetch(`${api}/posts`, { headers })
+        .then(res => res.json())
+        .then(data => {
+            //console.log('todos os posts')
+            //console.log(data)
+            return data
+        })
+        .catch(error => {
+            console.log(error)
+            return error
+        })
 
 export const getAllCategories = () =>
-  fetch(`${api}/categories`, { headers })
-    .then(res => res.json())
-    .then(data => {
-        //console.log('todas categorias')
-        //console.log(data)
-        return data
+    fetch(`${api}/categories`, {
+        method: 'get',
+        headers
     })
-    .catch(error =>{
-        console.log(error)
-        return error
-    })
+        .then(res => res.json())
+        .then(data => {
+            //console.log('todas categorias')
+            //console.log(data)
+            return data
+        })
+        .catch(error => {
+            console.log(error)
+            return error
+        })
 
 
 export const getPostsForCategories = (name) =>
-    fetch(`${api}/${name}/posts`, { headers })
-      .then(res => res.json())
-      .then(data => {
-        //console.log('posts com categorias',name)
-        //console.log(data)
-        return data
-      })
-      .catch(error =>{
-        console.log(error)
-        return error
+    fetch(`${api}/${name}/posts`, {
+        method: 'get',
+        headers
     })
+        .then(res => res.json())
+        .then(data => {
+            //console.log('posts com categorias',name)
+            //console.log(data)
+            return data
+        })
+        .catch(error => {
+            console.log(error)
+            return error
+        })
 
+export const getPostDetails = (postId) =>
+    fetch(`${api}/posts/${postId}`, {
+        method: 'get',
+        headers
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log("post com detalhes", postId)
+            console.log(data)
+            return data
+        })
+        .catch(error => {
+            console.log(error)
+            return error
+        })
 
 export default api;
