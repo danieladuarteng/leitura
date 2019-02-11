536d6c7246e8360f12778b7bb93d40e517841721
dasantos <daniela.santos@kestraa.com>
2019-02-11 09:57:50 -0200

    feat: create action and reducers for postDetails

--- frontend/src/actions/posts.js
+++ frontend/src/actions/posts.js
@@ -1,8 +1,16 @@
 export const RECEIVE_POSTS = 'RECEIVE_POSTS'
+export const POST_DETAILS = 'POST_DETAILS'
 
-export function receivePosts (posts){
-    return{
+export function receivePosts(posts) {
+    return {
         type: RECEIVE_POSTS,
         posts: posts
+    }
+}
+
+export function receivePostDetails(post) {
+    return{
+        type: POST_DETAILS,
+        post: post
     }
 }
--- frontend/src/actions/shared.js
+++ frontend/src/actions/shared.js
@@ -1,12 +1,23 @@
-import { getAllPosts } from '../LeituraAPI'
-import { receivePosts } from './posts'
+import { getAllPosts, getPostDetails } from '../LeituraAPI'
+import { receivePosts, receivePostDetails } from './posts'
 
 export function handleInitialData() {
     return (dispatch) => {
         return getAllPosts()
-            .then(posts  => {
+            .then((posts)  => {
             dispatch(receivePosts(posts))
         })
     }
 }
+
+
+export function postDetails(id){
+    return (dispatch) =>{
+        return getPostDetails(id)
+            .then(post =>{
+                dispatch(receivePostDetails(post))
+            })
+    }
+}
+
 
