0600dfd067a024ad80e3cd8098cf80e5633bcaa1
Daniela Duarte <falecom@danieladuarte.com.br>
2019-03-03 21:59:52 -0300

    refactor: reducer of editPost

--- frontend/src/reducers/post.js
+++ frontend/src/reducers/post.js
@@ -24,34 +24,36 @@ export default function post(state = {}, action) {
                 ]
             }
         case EDIT_POST:
-            const postId = Object.keys(state).find(key => {
-                if (state[key].id === action.post.id)
-                    return key
+            const postId = Object.keys(state).filter(item => 
+                state[item].id === action.post.id)
+                
 
                 return {
-                    post: [...key],
-                    ...action.post,
                     ...state,
-                    posts: [...postId]
+                    [postId]:{
+                        ...state[postId],
+                        post: state.post
+                    }
+                    
                 }
-            })
+            
 
-        case NEW_COMMENT:
-            const { comment } = action
-            return {
-                ...state,
-                commentCount: state.commentCount + 1,
-                comments: state.comment.push(action.comment)
-            }
-        case TOGGLE_VOTE_SCORE:
-            const { post } = action
-            return {
-                ...state,//retorno os posts antigos
-                [post.id]: {
-                    ...state[post.id],
-                    voteScore: action.vote === 'downVote' ? post.voteScore - 1 : post.voteScore + 1
-                }
-            }
+        // case NEW_COMMENT:
+        //     const { comment } = action
+        //     return {
+        //         ...state,
+        //         commentCount: state.commentCount + 1,
+        //         comments: state.comment.push(action.comment)
+        //     }
+        // case TOGGLE_VOTE_SCORE:
+        //     const { post } = action
+        //     return {
+        //         ...state,//retorno os posts antigos
+        //         [post.id]: {
+        //             ...state[post.id],
+        //             voteScore: action.vote === 'downVote' ? post.voteScore - 1 : post.voteScore + 1
+        //         }
+        //     }
         default:
             return state
     }
