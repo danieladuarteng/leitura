b592f733bba92847b30a44b10e0741b937e1640f
Daniela Duarte <dani.baila@yahoo.com.br>
2019-03-05 18:00:54 -0300

    feat: create component Page404

---
+++ frontend/src/components/Page404.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Page404 = () => {
+  return (
+    <div>
+      <div className="home">
+        <h1><strong>Leitura</strong></h1>
+      </div>
+
+      <div className="grid-posts">
+        <div className="grid-posts-item1">
+          This post doesn't exist
+          </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page404
