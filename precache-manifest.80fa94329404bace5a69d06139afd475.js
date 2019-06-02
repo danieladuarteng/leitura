42903431fffe06f880da5088d730d9c3d179713b
Daniela Duarte <dani.baila@yahoo.com.br>
2019-06-01 22:45:03 -0300

    Updates

---
+++ precache-manifest.80fa94329404bace5a69d06139afd475.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "772d4252a925544ec527",
+    "url": "/leitura/static/js/runtime~main.772d4252.js"
+  },
+  {
+    "revision": "557ffdca93b57e029996",
+    "url": "/leitura/static/js/main.557ffdca.chunk.js"
+  },
+  {
+    "revision": "b8a9189d8d692ca01b07",
+    "url": "/leitura/static/js/2.b8a9189d.chunk.js"
+  },
+  {
+    "revision": "557ffdca93b57e029996",
+    "url": "/leitura/static/css/main.e4ca628d.chunk.css"
+  },
+  {
+    "revision": "083f737ba60f8139ec2fa5a3bdb7513f",
+    "url": "/leitura/index.html"
+  }
+];
